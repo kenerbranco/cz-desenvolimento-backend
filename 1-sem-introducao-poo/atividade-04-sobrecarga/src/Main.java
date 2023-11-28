d8f78f639a5810333a04a67f60e60d948a918c8d
kenerbranco <kenerbranco@hotmail.com>
2023-11-28 00:47:17 -0300

    atividade 04 finalizado - sobrecarga

---
+++ 1-sem-introducao-poo/atividade-04-sobrecarga/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Operadores calculoUm = new Operadores();
+        calculoUm.multiplicar(10, 20);
+
+        Operadores calculoDois = new Operadores();
+        calculoDois.multiplicar(2.5f, 3.5f);
+    }
+}
---
+++ 1-sem-introducao-poo/atividade-04-sobrecarga/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public void multiplicar(int numeroUm, int numeroDois){
+        System.out.println("Resultado inteiro: " + numeroUm * numeroDois);
+    }
+
+    public void multiplicar(float numeroUm, float numeroDois){
+        System.out.println("Resultado ponto flutuante: " + numeroUm * numeroDois);
+    }
+}
